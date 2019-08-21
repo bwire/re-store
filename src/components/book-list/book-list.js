7a8e03a106f66d93e6e21464b55ab1b5e61a753b
Yevheniy Gandzyuck <evgeniy.gandzyuck@area9.dk>
2019-08-21 15:12:58 +0300

    error handling

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,40 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import { connect } from 'react-redux';
 
 import BookListItem from '../book-list-item';
 import withBookstoreService from '../hoc';
-import { booksLoaded, booksRequested } from '../../actions';
+import { booksLoaded, booksRequested, booksError } from '../../actions';
 import { compose } from '../../services/utils';
 import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator';
 
 import './book-list.css';
 
 class BookList extends Component {
   componentDidMount() {
-    const { bookstoreService, booksLoaded, booksRequested } = this.props;
+    const { bookstoreService, booksLoaded, booksRequested, booksError } = this.props;
     booksRequested();
     bookstoreService.getBooks()
-      .then((data) => booksLoaded(data));
+      .then((data) => booksLoaded(data))
+      .catch((error) => booksError(error));
   }
 
   render() {
-    const { books, loading } = this.props;
-    if (loading)
+    const { books, loading, error } = this.props;
+    if (loading) {
       return <Spinner />;
-    else 
-      return (
-        <ul className="book-list">
-          { books.map(book => <li key={ book.id }><BookListItem book={ book } /></li>) }
-        </ul>
-      );
+    }
+
+    if (error) {
+      return <ErrorIndicator />;
+    }
+     
+    return (
+      <ul className="book-list">
+        { books.map(book => <li key={ book.id }><BookListItem book={ book } /></li>) }
+      </ul>
+    );
   }
 };
 
-const mapStateToProps = ({ books, loading }) => {
-  return { books, loading };
+const mapStateToProps = ({ books, loading, error }) => {
+  return { books, loading, error };
 }
 
 export default compose(
   withBookstoreService(), 
-  connect(mapStateToProps, { booksLoaded, booksRequested })
+  connect(mapStateToProps, { booksLoaded, booksRequested, booksError })
 )(BookList);
