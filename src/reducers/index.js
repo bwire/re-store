fe69814d87e047410ef016c192639d4332ee69f6
Yevheniy Gandzyuck <evgeniy.gandzyuck@area9.dk>
2019-09-04 11:03:28 +0300

    update cart

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,6 +6,25 @@ const initialState = {
   orderTotal: 350
 };
 
+const updateCartItem = (book, item = {}) => {
+  const { id = book.id, title = book.title, count = 0, total = 0 } = item;
+  return {
+    id: id,
+    title: title,
+    count: count + 1,
+    total: total + book.price
+  };
+}
+
+const updateCartItems = (state, id) => {
+  const book = state.books.find((book) => book.id === id);
+  const idx = state.cartItems.findIndex(item => item.id === book.id);    
+  const newItem = updateCartItem(book, state.cartItems[idx]);
+  return idx >= 0 
+    ? [...state.cartItems.slice(0, idx), newItem, ...state.cartItems.slice(idx + 1)]
+    : [...state.cartItems, newItem];
+}
+
 const reducer = (state = initialState, action) => {
   switch(action.type) {
     case 'FETCH_BOOKS_SUCCESS':
@@ -30,21 +49,9 @@ const reducer = (state = initialState, action) => {
         error: action.payload,
       };
     case 'BOOK_ADDED_TO_CART':
-      const id = action.payload;
-      const book = state.books.find((book) => book.id === id);
-      const newItem = {
-        id: book.id,
-        name: book.title,
-        count: 1,
-        total: book.price
-      };
-      
       return {
         ...state,
-        cartItems: [
-          ...state.cartItems,
-          newItem
-        ]
+        cartItems: updateCartItems(state, action.payload)
       };
       
     default: 
